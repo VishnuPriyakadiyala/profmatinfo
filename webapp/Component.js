761cd80aceb49f4d5dc2e60dcdab8d341dce109e
Vishnu Priya K <priya.vishnu525@gmail.com>
2023-04-21 13:50:44 +0000

    first commit

---
+++ webapp/Component.js
@@ -0,0 +1,12 @@
+sap.ui.define(
+    ["sap/suite/ui/generic/template/lib/AppComponent"],
+    function (Component) {
+        "use strict";
+
+        return Component.extend("sap.ui.mdm/mat.prodmatinfo.Component", {
+            metadata: {
+                manifest: "json"
+            }
+        });
+    }
+);
